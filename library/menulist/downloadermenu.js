cb4a129cdcddc57f2840bcd045595654cb45cbe8
toosiike <toosii042@gmail.com>
2026-03-17 19:42:27 +0300

    fix: escape inner backticks to \' in template literals (downloadermenu.js)

--- library/menulist/downloadermenu.js
+++ library/menulist/downloadermenu.js
@@ -2,17 +2,17 @@ const downloaderMenu = `
 ╔══════════════════════════╗
 ║  📥  *DOWNLOADERS*
 ╚══════════════════════════╝
-  ├◈  `.play`        › [query]
-  ├◈  `.song`        › [query]
-  ├◈  `.video`       › [query/url]
-  ├◈  `.ytdocplay`   › [query]
-  ├◈  `.ytdocvideo`  › [query]
-  ├◈  `.ig`          › [url]
-  ├◈  `.tt`          › [url]
-  ├◈  `.fb`          › [url]
-  ├◈  `.mediafire`   › [url]
-  ├◈  `.spotify`     › [song name]
-  ├◈  `.apk`         › [app name]
-  └◈  `.gitclone`    › [github url]`
+  ├◈  \`.play\`        › [query]
+  ├◈  \`.song\`        › [query]
+  ├◈  \`.video\`       › [query/url]
+  ├◈  \`.ytdocplay\`   › [query]
+  ├◈  \`.ytdocvideo\`  › [query]
+  ├◈  \`.ig\`          › [url]
+  ├◈  \`.tt\`          › [url]
+  ├◈  \`.fb\`          › [url]
+  ├◈  \`.mediafire\`   › [url]
+  ├◈  \`.spotify\`     › [song name]
+  ├◈  \`.apk\`         › [app name]
+  └◈  \`.gitclone\`    › [github url]`
 
 module.exports = downloaderMenu
